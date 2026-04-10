d2070497f91370f39b3a7e4028e1cd76370df033
losion <losion445@gmail.com>
2026-04-10 22:21:26 +0300

    [add] Config loading, config structure

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/losion445-max/telegram-bot-apo-golang
 
 go 1.26.1
+
+require (
+	github.com/BurntSushi/toml v1.6.0
+	github.com/pelletier/go-toml/v2 v2.3.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,100 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
 
+	"github.com/BurntSushi/toml"
+)
+
+// Constants
+var (
+	BaseUrl        = "https://api.telegram.org"
+	Command        = "sendMessage"
+	DefaultMessage = "Ding!"
+)
+
+// Config
+type Config struct {
+	Bot_token string
+	Chat_ID   string
+}
+
+func LoadConfig() (*Config, error) {
+	confDir, err := os.UserConfigDir()
+	if err != nil {
+		return nil, err
+	}
+
+	confPath := filepath.Join(confDir, "tg_notify", "conf.toml")
+
+	conf, err := os.ReadFile(confPath)
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't load config at %s", confPath)
+	}
+
+	var config Config
+	err = toml.Unmarshal(conf, &config)
+	if err != nil {
+		return nil, fmt.Errorf("Invalid conf.toml %s", &config)
+	}
+
+	return &config, nil
+}
+
+// Functions
+type SendMessageDTO struct {
+	ChatID string `json:"chat_id"`
+	Text   string `json:"text"`
+}
+
+func sendMessage(config *Config, m SendMessageDTO) error {
+	base, err := url.Parse(BaseUrl)
+	if err != nil {
+		return errors.New("internal error")
+	}
+
+	base = base.JoinPath("bot" + config.Bot_token).JoinPath(Command)
+
+	jsonData, err := json.Marshal(m)
+	if err != nil {
+		return errors.New("internal error")
+	}
+
+	body := bytes.NewBuffer(jsonData)
+	resp, err := http.Post(base.String(), "application/json", body)
+	if err != nil {
+		return errors.New("internal error")
+	}
+	defer resp.Body.Close()
+
+	return nil
+
+}
+
+// Entrypoint
 func main() {
-	fmt.Println("[RUN] main.go")
+	config, err := LoadConfig()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	msg := SendMessageDTO{
+		config.Chat_ID,
+		DefaultMessage,
+	}
+
+	err = sendMessage(config, msg)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
 }
